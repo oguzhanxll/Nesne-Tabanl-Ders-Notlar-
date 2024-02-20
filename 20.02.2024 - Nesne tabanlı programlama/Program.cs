3ee65cf45a28f60c5377f5e6ff39916c084b9b97
casper <casper@OUZHAN-PC>
2024-02-20 11:51:01 +0300

    Proje dosyası ekle.

---
+++ 20.02.2024 - Nesne tabanlı programlama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._02._2024___Nesne_tabanlı_programlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* object x;
+            x = 10;
+            Console.WriteLine(x.GetType());
+            x = "B";
+            Console.WriteLine(x.GetType());
+            x = 8.78d;
+            Console.WriteLine(x.GetType());
+            x = false;
+            Console.WriteLine(x.GetType());
+            x = 5.489M;
+            Console.WriteLine(x.GetType());
+            x = 'a';
+            Console.WriteLine(x.GetType());
+
+            Console.ReadLine();
+
+            */ 
+            
+            //() = Varsa metod anlamına gelir.
+
+            
+
+            
+        }
+    }
+}
